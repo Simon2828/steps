f0be63a1248c663e3a5c0f73d516ed116cccd590
simon2828 <simon.tanner28@gmail.com>
2017-12-27 15:27:52 +0000

    updated routes for ghpages

--- src/routes.js
+++ src/routes.js
@@ -4,11 +4,12 @@ import App from './containers/App';
 import About from './containers/About';
 
 export default () => {
+	console.log('process.env.PUBLIC_URL:', process.env.PUBLIC_URL)
 	return (
 		<BrowserRouter>
 			<Switch>
 			  <Route exact path={process.env.PUBLIC_URL + '/'} component={App}/>
-			  <Route path={process.env.PUBLIC_URL + '/steps-to-success'} component={About}/>
+			  <Route path={process.env.PUBLIC_URL + '/steps/steps-to-success'} component={About}/>
 			</Switch>
 		</BrowserRouter>
 	)
