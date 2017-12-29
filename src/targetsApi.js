5acb52c826362d44cdce15b1bb797329bb078f01
simon2828 <simon.tanner28@gmail.com>
2017-12-29 13:22:21 +0000

    updated styles

--- src/targetsApi.js
+++ src/targetsApi.js
@@ -23,6 +23,14 @@
       {
       lO: "Write a poem",
       stepsToSuccess: ["Include verses", "Use a simile"]
+    },
+    {
+      lO: "Write about a setting",
+      stepsToSuccess: ["Include the time of day or year", "Describe what the weather is like", "Describe what you can see, hear and feel"]
+    },
+    {
+      lO: "Write instructions",
+      stepsToSuccess: ["Used bossy verbs", "Used conjunctions to join two ideas together", "Used connectives to make the order clear"]
     }
   ]
     
