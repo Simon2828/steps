023288ff10156ed32b6298bbb19f2eca2d846990
simon2828 <simon.tanner28@gmail.com>
2018-07-23 16:58:25 +0100

    update resources

--- backend/list.js
+++ backend/list.js
@@ -4,17 +4,20 @@ import { success, failure } from "./lib/response-lib";
 export async function main(event, context, callback) {
   console.log('event', event)
   console.log('callback', callback)
+  console.log('process.env', process.env)
     const params = {
-      TableName: process.env.tableName,
+      TableName: "dev-learningObjectives",
+      // value for TableName above:   process.env.tableName
+
       // 'KeyConditionExpression' defines the condition for the query
       // - 'userId = :userId': only return items with matching 'userId'
       //   partition key
       // 'ExpressionAttributeValues' defines the value in the condition
       // - ':userId': defines 'userId' to be Identity Pool identity id
       //   of the authenticated user
-      KeyConditionExpression: "learningObjectiveId = :learningObjectiveId",
+      KeyConditionExpression: "learningObjective = :learningObjective",
       ExpressionAttributeValues: {
-        ":learningObjectiveId": "1"
+        ":learningObjective": "1"
       }
     };
   
